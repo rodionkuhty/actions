034da10ae02cf63d244bc07e620ff4c1e4191aea
Kukhty, Rodion <rodionkuhty@gmail.com>
2025-03-07 17:26:25 +0300

    node

--- e2e-tests/tests/example.spec.ts
+++ e2e-tests/tests/example.spec.ts
@@ -1,18 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:3000');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  const firstPoint = await page.getByText('Get started by editing src/pages/index.tsx', { exact: false })
+  const secondPoint = await page.getByText('Save and see your changes instantly', { exact: false })
+  await expect(firstPoint).toBeVisible();
+  await expect(secondPoint).toBeVisible();
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:3000');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(await page.getByRole('link', { name: 'Deploy now' })).toBeVisible()
+  await expect(await page.getByRole('link', { name: 'Read our docs' })).toBeVisible()
+  // await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
