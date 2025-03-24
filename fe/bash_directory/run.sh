6f48cb3880715fe0c732962c234de78217e351ce
Kukhty, Rodion <rodionkuhty@gmail.com>
2025-03-24 17:28:08 +0300

    docker in ci

---
+++ fe/bash_directory/run.sh
@@ -0,0 +1,13 @@
+echo "Hello github actions from docker container"
+echo "This is a bash script running inside a Docker container."
+echo "Current date and time: $(date)"
+echo "This script is running in the directory: $(pwd)"
+echo "List of files in the current directory:"
+ls -l
+echo "End of script."
+# This script prints a greeting message, the current date and time, the current directory, and lists the files in that directory.
+# It serves as a simple demonstration of running a bash script inside a Docker container.
+# It can be used for testing and automation purposes in a CI/CD pipeline.
+# The script is executed when the Docker container is run, showcasing the functionality of the container.
+# The script is executed when the Docker container is run, showcasing the functionality of the container.
+# The script is executed when the Docker container is run, showcasing the functionality of the container.
