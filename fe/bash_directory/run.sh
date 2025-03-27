7f00da896558987dd7591effb471b44e6ad7091c
Kukhty, Rodion <rodionkuhty@gmail.com>
2025-03-27 22:45:55 +0300

    network3

--- fe/bash_directory/run.sh
+++ fe/bash_directory/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-echo "Hello github actions from docker container"
-echo "This is a bash script running inside a Docker container."
-echo "Current date and time: $(date)"
-echo "This script is running in the directory: $(pwd)"
-echo "List of files in the current directory:"
+echo "start"
 ls -l
 echo "End of script."
 curl http://playwright:3000
