69eda65066a98518dbafd1816abd64f27fd0c189
Kukhty, Rodion <rodionkuhty@gmail.com>
2025-03-28 16:59:39 +0300

    tests

--- e2e-tests/tests/example.spec.ts
+++ e2e-tests/tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('http://frontend:3000/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Create Next App/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('http://frontend:3000/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Deploy Now' }).click();
