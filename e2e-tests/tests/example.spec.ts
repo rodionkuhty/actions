b8624da5b5e37f9669f1f2a90cfe4c974424b4d6
Kukhty, Rodion <rodionkuhty@gmail.com>
2025-05-05 16:36:50 +0300

    fix3

--- e2e-tests/tests/example.spec.ts
+++ e2e-tests/tests/example.spec.ts
@@ -1,16 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Create Next App/);
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-await expect(page.getByText('Get started', {  exact: false })).toBeVisible();
-await expect(page.getByText('Save and see', {  exact: false })).toBeVisible();
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
