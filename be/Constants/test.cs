55cb531aa77dd2e06066721f6bdc4f9d376b324b
rodionkuhty <rodionkuhty@users.noreply.github.com>
2025-05-05 21:31:18 +0000

    deploy: 8246dce6254abf43139c8cf9b0f87d5968c17ba8

--- be/Constants/test.cs
+++
@@ -1,9 +0,0 @@
-namespace be.Constants;
-
-public static class Constants
-{
-    public static readonly string[] Summaries =
-    [
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    ];
-}
--- be/records/index.cs
+++
@@ -1,5 +0,0 @@
-namespace be.Records;
-record Test(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
